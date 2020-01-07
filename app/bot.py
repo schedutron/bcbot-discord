e01394278e2a6924c41316fd3cdba516999450da
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2020-01-07 19:46:21 +0530

    Clean up the code a bit

--- app/bot.py
+++ app/bot.py
@@ -11,6 +11,23 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="$")
 invoker_map = {}
+# [:speaking_head:] To Online\n"
+#              "[:satellite:] To All\n"
+#              "[:microphone2:] To Role",
+#         name='\u200b',
+#         inline=True
+#     )
+#     embed.add_field(
+#         value="[:loudspeaker:] To Online (embed)\n"
+#              "[:satellite_orbital:] To All (embed)\n"
+#              "[:no_entry_sign:] Cancel",
+# ['\U0001F5E3', '\U0001F4E1', '\U0001F399', '\U0001F4E2', '\U0001F6F0', '\U0001F6AB']:
+options = {
+    '\U0001F5E3': 'To Online', '\U0001F4E1': 'To All',
+    '\U0001F399': 'To Role', '\U0001F4E2': 'To Online (Embed)',
+    '\U0001F6F0': 'To All (Embed)', '\U0001F6AB': 'Cancel'
+    }
+
 
 @bot.event
 async def on_ready():
@@ -34,21 +51,21 @@ async def bcbot(ctx, *, broadcast_msg):
     #print(time.ctime())
     embed = discord.Embed(title="New Broadcast", color=0x000000)
     embed.add_field(
-        value="[:speaking_head:] To Online\n"
-             "[:satellite:] To All\n"
-             "[:microphone2:] To Role",
+        value="\n".join(
+            [f"[{k}] {v}"  for k, v in list(options.items())[:3]]
+            ),
         name='\u200b',
         inline=True
     )
     embed.add_field(
-        value="[:loudspeaker:] To Online (embed)\n"
-             "[:satellite_orbital:] To All (embed)\n"
-             "[:no_entry_sign:] Cancel",
+        value="\n".join(
+            [f"[{k}] {v}"  for k, v in list(options.items())[3:]]
+            ),
         name='\u200b',
         inline=True
     )
 
-    embed.set_author(name=author.display_name, icon_url=author.avatar_url)
+    embed.set_author(name=f"{author}", icon_url=author.avatar_url)
     embed.set_footer(text="Please react with an appropriate action to continue")
     if ctx.message.channel.id not in invoker_map:
         invoker_map[ctx.message.channel.id] = {}
@@ -58,10 +75,12 @@ async def bcbot(ctx, *, broadcast_msg):
     }
     await ctx.send(embed=embed)
     msg = await ctx.channel.history().get(author__name=bot.user.display_name)
-    # Manually adding reactions for now
-    for emoji in ['\U0001F5E3', '\U0001F4E1', '\U0001F399', '\U0001F4E2', '\U0001F6F0', '\U0001F6AB']:
+
+    for emoji in options.keys():
         await msg.add_reaction(emoji)
+
     print(invoker_map)
+    print('------------------')
     await asyncio.sleep(60)
     del invoker_map[msg.channel.id][author.id]  # Delete entry on timeout
     await msg.delete()
@@ -87,6 +106,11 @@ async def bcbot(ctx, *, broadcast_msg):
 
     print(time.ctime())
     await ctx.send(f"Broadcast ended at: {time.ctime()}")
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    print(reaction, user)
 
 
 if __name__ == '__main__':
